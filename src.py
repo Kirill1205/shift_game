ee0a07c30c248a9af38ef3f2f205f21a4cf71ade
Dmitry Luschan <dluschan@gmail.com>
2017-05-13 22:07:43 +0300

    использование функции compress

--- src.py
+++ src.py
@@ -2,84 +2,63 @@ import tkinter, random
 
 size = 4
 
+def compress(s):
+    s = list(filter(lambda x: x != 0, s))
+    i = 0
+    while i < len(s) - 1:
+        if s[i] == s[i+1]:
+            s[i] *= 2
+            del s[i+1]
+        i += 1
+    while len(s) < size:
+        s.append(0)
+    return s
+
 def shift(event, d):
     if d == 1:
         for row in range(size):
             s = []
             for col in range(size):
                 p = cells[col + size * row]['text']
-                if p != '':
-                    s.append(int(p))
-            s = s[::-1]
-            i = 0
-            while i < len(s) - 1:
-                if s[i] == s[i+1]:
-                    s[i] *= 2
-                    del s[i+1]
-                i += 1
-            s = s[::-1]
+                s.append(int(p) if p != '' else 0)
+            s = compress(s[::-1])[::-1]
             for col in range(size):
-                if col < (size - len(s)):
-                    cells[col + size * row]['text'] = ''
-                else:
-                    cells[col + size * row]['text'] = s[col - (size - len(s))]
+                cells[col + size * row]['text'] = s[col] if s[col] != 0 else ''
     if d == 3:
         for row in range(size):
             s = []
             for col in range(size):
                 p = cells[col + size * row]['text']
-                if p != '':
-                    s.append(int(p))
-            i = 0
-            while i < len(s) - 1:
-                if s[i] == s[i+1]:
-                    s[i] *= 2
-                    del s[i+1]
-                i += 1
+                s.append(int(p) if p != '' else 0)
+            s = compress(s)
             for col in range(size):
-                if col < len(s):
-                    cells[col + size * row]['text'] = s[col]
-                else:
-                    cells[col + size * row]['text'] = ''
+                cells[col + size * row]['text'] = s[col] if s[col] != 0 else ''
     if d == 0:
         for col in range(size):
             s = []
             for row in range(size):
                 p = cells[col + size * row]['text']
-                if p != '':
-                    s.append(int(p))
-            i = 0
-            while i < len(s) - 1:
-                if s[i] == s[i+1]:
-                    s[i] *= 2
-                    del s[i+1]
-                i += 1
+                s.append(int(p) if p != '' else 0)
+            s = compress(s)
             for row in range(size):
-                if row < len(s):
-                    cells[col + size * row]['text'] = s[row]
-                else:
-                    cells[col + size * row]['text'] = ''
+                cells[col + size * row]['text'] = s[row] if s[row] != 0 else ''
     if d == 2:
         for col in range(size):
             s = []
             for row in range(size):
                 p = cells[col + size * row]['text']
-                if p != '':
-                    s.append(int(p))
-            s = s[::-1]
-            i = 0
-            while i < len(s) - 1:
-                if s[i] == s[i+1]:
-                    s[i] *= 2
-                    del s[i+1]
-                i += 1
-            s = s[::-1]
+                s.append(int(p) if p != '' else 0)
+            s = compress(s[::-1])[::-1]
             for row in range(size):
-                if row < (size - len(s)):
-                    cells[col + size * row]['text'] = ''
-                else:
-                    cells[col + size * row]['text'] = s[row - (size - len(s))]
+                cells[col + size * row]['text'] = s[row] if s[row] != 0 else ''
     generate()
+
+def over(event):
+    step = 0
+    while step < 1000:
+        print(step)
+        shift(event, step % 4)
+        step += 1
 
 def generate():
     free_cells = []
@@ -100,6 +79,7 @@ frame.bind('<Up>'   , lambda e: shift(e, 0))
 frame.bind('<Right>', lambda e: shift(e, 1))
 frame.bind('<Down>' , lambda e: shift(e, 2))
 frame.bind('<Left>' , lambda e: shift(e, 3))
+frame.bind('<R>' , lambda e: over(e))
 
 cells = []
 for i in range(size**2):
