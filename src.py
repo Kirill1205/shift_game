6b2f0bdbc23b285de03437e5b35b4136daaa1c75
Dmitry Luschan <dluschan@gmail.com>
2017-05-20 11:46:27 +0300

    добавлена возможность откатывать сделанные ходы после игры

--- src.py
+++ src.py
@@ -2,6 +2,9 @@ import tkinter, random
 
 size = 4
 score = 0
+states = []
+state_id = None
+game_cont = True
 
 def compress(s):
     s = list(filter(lambda x: x != 0, s))
@@ -40,6 +43,16 @@ def shift_src(by_index, comp_fun):
             cells[by_index(x, y)]['text'] = compressed_s[y] if compressed_s[y] != 0 else ''
     return result
 
+def save_state():
+    state = []
+    for x in range(size):
+        s = []
+        for y in range(size):
+            p = cells[by_row(x, y)]['text']
+            s.append(int(p) if p != '' else 0)
+        state.append(s)
+    states.append(state)
+
 def is_game_over():
     for direction in data:
         by_index = direction[0]
@@ -55,12 +68,15 @@ def is_game_over():
     return True
 
 def game_over():
+    global game_cont
+    game_cont = False
     print("Game over!")
     print("Your maxumim number:", max(map(lambda x: int(x['text']), cells)))
     print("Your score:", score)
 
 def shift(event, d):
     if shift_src(data[d][0], data[d][1]):
+        save_state()
         generate()
         return True
     else:
@@ -100,6 +116,17 @@ def generate():
     else:
         random.choice(free_cells)['text'] = random.choice(['2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '4'])
 
+def roll_back(event):
+    if game_cont:
+        return
+    global state_id
+    if state_id == None:
+        state_id = len(states) - 1
+    state_id -= 1
+    for x in range(size):
+        for y in range(size):
+            cells[by_row(x, y)]['text'] = str(states[state_id][x][y]) if states[state_id][x][y] != 0 else ''
+
 root = tkinter.Tk()
 frame = tkinter.Frame(root)
 frame.focus_set()
@@ -110,6 +137,7 @@ frame.bind('<Down>' , lambda e: shift(e, 2))
 frame.bind('<Left>' , lambda e: shift(e, 3))
 frame.bind('<space>', lambda e: over_l_d(e))
 frame.bind('<r>'    , lambda e: over_r  (e))
+frame.bind('<z>'    , lambda e: roll_back(e))
 
 cells = []
 for i in range(size**2):
